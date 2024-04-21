ba2991b1f6d931fe540aeaad5ea251f3c81d190a
hanafy <mohamed_hassan_hanafy@outlook.com>
2024-04-22 00:07:07 +0200

    Add solution #3 Kids With Greatest Number of Candies

---
+++ src/main/java/me/_3KidsWithGreatestNumberCandies.java
@@ -0,0 +1,18 @@
+package me;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _3KidsWithGreatestNumberCandies {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // use simple for loop operation for getting max will improve performance as stream use comparator and boxing
+        int maxCandies = Arrays.stream(candies).max().orElse(Integer.MAX_VALUE);
+        List<Boolean> result = new ArrayList<>(candies.length);
+        for (int i = 0; i < candies.length; i++) {
+            result.add(i, candies[i] + extraCandies >= maxCandies);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/me/_3KidsWithGreatestNumberCandiesTest.java
@@ -0,0 +1,53 @@
+package me;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _3KidsWithGreatestNumberCandiesTest {
+
+    @Test
+    void kidsWithCandies_case1() {
+        //given
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+
+        //when
+        _3KidsWithGreatestNumberCandies underTest = new _3KidsWithGreatestNumberCandies();
+        List<Boolean> result = underTest.kidsWithCandies(candies, extraCandies);
+        //then
+        Boolean[] expected = {true,true,true,false,true};
+        assertEquals(Arrays.asList(expected), result);
+    }
+
+    @Test
+    void kidsWithCandies_case2() {
+        //given
+        int[] candies = {4,2,1,1,2};
+        int extraCandies = 1;
+
+        //when
+        _3KidsWithGreatestNumberCandies underTest = new _3KidsWithGreatestNumberCandies();
+        List<Boolean> result = underTest.kidsWithCandies(candies, extraCandies);
+        //then
+        Boolean[] expected = {true,false,false,false,false};
+        assertEquals(Arrays.asList(expected), result);
+    }
+
+    @Test
+    void kidsWithCandies_case3() {
+        //given
+        int[] candies = {12,1,12};
+        int extraCandies = 10;
+
+        //when
+        _3KidsWithGreatestNumberCandies underTest = new _3KidsWithGreatestNumberCandies();
+        List<Boolean> result = underTest.kidsWithCandies(candies, extraCandies);
+        //then
+        Boolean[] expected = {true,false,true};
+        assertEquals(Arrays.asList(expected), result);
+    }
+}
