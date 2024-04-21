4db36677b72d442eb984a3c5b621765a972e720f
hanafy <mohamed_hassan_hanafy@outlook.com>
2024-04-21 22:50:13 +0200

    Add solution #2 Greatest Common Divisor Strings

---
+++ src/main/java/me/_2GreatestCommonDivisorStrings.java
@@ -0,0 +1,27 @@
+package me;
+
+public class _2GreatestCommonDivisorStrings {
+
+    public String gcdOfStrings(String str1, String str2) {
+
+        String minString = str1.length() >= str2.length() ? str2 : str1;
+
+        String greatestCommon = "";
+        for(int i= minString.length(); i >= 1; i--){
+            String toBeGreatestCommon = minString.substring(0, i);
+            // check reminder is zero for result of split of str1 & str2 i.e this is common between both of them
+            int splitLengthStr1 = str1.split(toBeGreatestCommon, -1).length-1;
+            int splitLengthStr2 = str2.split(toBeGreatestCommon, -1).length-1;
+            if(splitLengthStr1 == 0 || splitLengthStr2 == 0)
+                continue;
+
+            if(str1.length() == toBeGreatestCommon.length() * splitLengthStr1 &&
+                    str2.length() == toBeGreatestCommon.length() * splitLengthStr2  ){
+                greatestCommon = toBeGreatestCommon;
+                break;
+            }
+        }
+
+        return greatestCommon;
+    }
+}
---
+++ src/test/java/me/_2GreatestCommonDivisorStringsTest.java
@@ -0,0 +1,65 @@
+package me;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _2GreatestCommonDivisorStringsTest {
+
+    @Test
+    void gcdOfStrings_1() {
+
+        //given
+        String word1 = "ABCABC";
+        String word2 = "ABC";
+
+        //when
+        _2GreatestCommonDivisorStrings underTest = new _2GreatestCommonDivisorStrings();
+        String result = underTest.gcdOfStrings(word1, word2);
+        //then
+        assertEquals("ABC", result);
+    }
+
+
+    @Test
+    void gcdOfStrings_2() {
+
+        //given
+        String word1 = "ABABAB";
+        String word2 = "AB";
+
+        //when
+        _2GreatestCommonDivisorStrings underTest = new _2GreatestCommonDivisorStrings();
+        String result = underTest.gcdOfStrings(word1, word2);
+        //then
+        assertEquals("AB", result);
+    }
+
+    @Test
+    void gcdOfStrings_3() {
+
+        //given
+        String word1 = "ABABABAB";
+        String word2 = "ABAB";
+
+        //when
+        _2GreatestCommonDivisorStrings underTest = new _2GreatestCommonDivisorStrings();
+        String result = underTest.gcdOfStrings(word1, word2);
+        //then
+        assertEquals("ABAB", result);
+    }
+
+    @Test
+    void gcdOfStrings_4() {
+
+        //given
+        String word1 = "LEET";
+        String word2 = "CODE";
+
+        //when
+        _2GreatestCommonDivisorStrings underTest = new _2GreatestCommonDivisorStrings();
+        String result = underTest.gcdOfStrings(word1, word2);
+        //then
+        assertEquals("", result);
+    }
+}
