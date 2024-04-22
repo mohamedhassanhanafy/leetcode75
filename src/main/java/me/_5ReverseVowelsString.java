06aa4e8891828cba07bf90733113ef155438804e
hanafy <mohamed_hassan_hanafy@outlook.com>
2024-04-22 23:53:36 +0200

    Add solution #5 Reverse Vowels String

---
+++ src/main/java/me/_5ReverseVowelsString.java
@@ -0,0 +1,48 @@
+package me;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class _5ReverseVowelsString {
+
+    public String reverseVowels(String s) {
+
+        int leftIndex = 0;
+        int rightIndex = s.length()-1;
+        String vowels = "aeiouAEIOU"; // I used first List<Character> and contains which adds of cpu time use index better
+        char[] s_arr = s.toCharArray();
+
+        boolean isLeftCharVowel = false, isRightCharVowel = false;
+        while (leftIndex < rightIndex){
+
+            //swap condition
+            if(isLeftCharVowel && isRightCharVowel){
+                char temp = s_arr[leftIndex];
+                s_arr[leftIndex] = s_arr[rightIndex];
+                s_arr[rightIndex] = temp;
+                leftIndex ++;
+                rightIndex--;
+                isLeftCharVowel = false;
+                isRightCharVowel = false;
+            }
+
+            if (vowels.indexOf(s_arr[leftIndex]) > -1){
+                isLeftCharVowel = true;
+            } else{
+                leftIndex++;
+            }
+
+            if (vowels.indexOf(s_arr[rightIndex]) > -1){
+                isRightCharVowel = true;
+            }else {
+                rightIndex --;
+            }
+
+
+        }
+        return new String(s_arr);
+    }
+
+}
---
+++ src/test/java/me/_5ReverseVowelsStringTest.java
@@ -0,0 +1,76 @@
+package me;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _5ReverseVowelsStringTest {
+
+    @Test
+    void reverseVowels() {
+        //given
+        String s = "hello";
+
+        //when
+        _5ReverseVowelsString underTest = new _5ReverseVowelsString();
+        String result = underTest.reverseVowels(s);
+        //then
+        assertEquals("holle", result);
+
+    }
+
+    @Test
+    void reverseVowels_case2() {
+        //given
+        String s = "A man, a plan, a canal: Panama";
+
+        //when
+        _5ReverseVowelsString underTest = new _5ReverseVowelsString();
+        String result = underTest.reverseVowels(s);
+        //then
+        assertEquals("a man, a plan, a canal: PanamA", result);
+
+    }
+
+    @Test
+    void reverseVowels_caseOneCharVowel() {
+        //given
+        String s = "A";
+
+        //when
+        _5ReverseVowelsString underTest = new _5ReverseVowelsString();
+        String result = underTest.reverseVowels(s);
+        //then
+        assertEquals("A", result);
+
+    }
+
+    @Test
+    void reverseVowels_caseOneCharNotVowel() {
+        //given
+        String s = "B";
+
+        //when
+        _5ReverseVowelsString underTest = new _5ReverseVowelsString();
+        String result = underTest.reverseVowels(s);
+        //then
+        assertEquals("B", result);
+
+    }
+
+
+
+    @Test
+    void reverseVowels_caseRepeatedVowels() {
+        //given
+        String s = "AAAAAAAA";
+
+        //when
+        _5ReverseVowelsString underTest = new _5ReverseVowelsString();
+        String result = underTest.reverseVowels(s);
+        //then
+        assertEquals("AAAAAAAA", result);
+
+    }
+
+}
