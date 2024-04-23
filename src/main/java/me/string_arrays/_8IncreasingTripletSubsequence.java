d1d22424ae9a309605d3956c5b81f8887796d86f
hanafy <mohamed_hassan_hanafy@outlook.com>
2024-04-24 01:38:31 +0200

    Add solution #8 Increasing Triplet from society

--- src/main/java/me/string_arrays/_8IncreasingTripletSubsequence.java
+++ src/main/java/me/string_arrays/_8IncreasingTripletSubsequence.java
@@ -45,6 +45,29 @@ public class _8IncreasingTripletSubsequence {
 
         return false;
     }
+    // another simple solution
+    //The algorithm relies on the fact that if there is an increasing triplet in the array, there must be two values, a and b, such that a < b.
+    // The algorithm keeps track of the two smallest values encountered (a and b).
+    // If it finds an element greater than both a and b, it means there is an increasing triplet.
+    public boolean increasingTriplet_simple(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 }
