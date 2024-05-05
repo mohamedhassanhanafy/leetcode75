deff70db26817ee697f507419765dfafdf945feb
hanafy <mohamed_hassan_hanafy@outlook.com>
2024-05-05 09:49:12 +0200

    Add solutions #26 decoding String

--- src/main/java/me/stack/_26DecodingString.java
+++ src/main/java/me/stack/_26DecodingString.java
@@ -16,22 +16,51 @@ public class _26DecodingString {
         StringBuilder inputStr = new StringBuilder();
         LinkedList<Character> closingBrackets = new LinkedList<>();
         while (!stack.isEmpty()){
-            StringBuilder tempStr = new StringBuilder();
-            char currentChar = stack.pop();
-            if (currentChar == ']'){
-                closingBrackets.push(currentChar);
-            }else if (currentChar == '['){
-                int numberOfRepeat = stack.pop();
-            }else { // str
-                if(closingBrackets.isEmpty()){
-                    inputStr.append(tempStr);
-                }else {
-                    
+            Character currentElement = stack.pop();
+            if(currentElement.equals('[')){
+                // get number after [
+                // should be extracted to another method
+                String numberAfterBrackets = "";
+                while (!stack.isEmpty()){
+                    if("0123456789".contains(stack.peek().toString())){
+                        numberAfterBrackets = stack.pop() + numberAfterBrackets;
+                    }else {
+                        break;
+                    }
                 }
+                Integer numberOfRepeat = Integer.valueOf(numberAfterBrackets);
+
+
+                // extract from closing stack all elements till closing ]
+                StringBuilder repeatingStr = new StringBuilder();
+                while (!closingBrackets.isEmpty()){
+                    Character currentElementClosingStack = closingBrackets.pop();
+                    if(currentElementClosingStack.equals(']')){
+                        break;
+                    }else {
+                        repeatingStr.append(currentElementClosingStack);
+                    }
+                }
+
+                StringBuilder fullRepeatedString = new StringBuilder();
+                // create repeatinf String and push it again in closing Stack
+                for (int i = 0; i < numberOfRepeat; i++) {
+                    fullRepeatedString.append(repeatingStr);
+                }
+                for (int i = fullRepeatedString.length()-1 ; i > -1; i--) {
+                    closingBrackets.push(fullRepeatedString.charAt(i));
+                }
+
+
+            }else {
+                //
+                closingBrackets.push(currentElement);
             }
-
-
         }
-        return "";
+        while (!closingBrackets.isEmpty()){
+            inputStr.append(closingBrackets.pop());
+        }
+
+        return inputStr.toString();
     }
 }
